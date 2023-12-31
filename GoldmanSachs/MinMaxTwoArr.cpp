800daf39bb6a354c812d9994be77b4f9e12c519b
Arun J <96646536+Arunjagan12@users.noreply.github.com>
2023-12-31 17:44:37 +0530

    Update MinMaxTwoArr.cpp

--- GoldmanSachs/MinMaxTwoArr.cpp
+++ GoldmanSachs/MinMaxTwoArr.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+
+    int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2) {
+        long  mul  = ( long )divisor1*divisor2/gcd(divisor1, divisor2); 
+        int  lo  =  1 , hi= INT_MAX; 
+        // binary search 
+        while (lo<hi) { 
+            int  mid  = lo+(hi-lo)/ 2 ; 
+            if (mid -(mid/divisor1) >= uniqueCnt1 
+                && mid-(mid/divisor2) >=uniqueCnt2 
+                && mid-(mid/mul) >= uniqueCnt1+uniqueCnt2) { 
+                   
+                     hi = mid; 
+            } else 
+                 lo = mid+ 1 ; 
+        } 
+        return lo; 
+    }
+     int  gcd ( int a, int b) { 
+        if (a== 0 ) return b; 
+        return gcd(b%a, a); 
+    } 
+};
 
